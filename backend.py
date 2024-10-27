adf79199b2f4c3f446720eaf546fd452154ca427
cyxdev <ziadatef@192.168.1.7>
2024-10-27 15:24:47 -0700

    bot at 2024-10-27 15:24:47

---
+++ backend.py
@@ -0,0 +1,7 @@
+def showlogs():
+    loglist = [] 
+    loglist.append("$- Welcome, This is just comming from python.")
+    loglist.append("$- Yeah, this framework is about jst bridging python with web.")
+    loglist.append("$- We are super inspired by EEL.")
+    loglist.append("$- this is just the beginning. <3")
+    return loglist
