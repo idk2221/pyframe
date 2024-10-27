19ef6c1590fd3d7c3f75f3419752b170ff9657b7
cyxdev <ziadatef@192.168.1.7>
2024-10-27 16:29:25 -0700

    bot at 2024-10-27 16:29:25

--- websocket.py
+++ websocket.py
@@ -1,7 +1,13 @@
+import asyncio
 import websockets
 
-ws = websockets.connect("ws://localhost:8000/ws/test")
-async def wait():
-    recvv = await ws.recv()
-    return recvv
-print( wait())
+async def wss():
+    try:
+        async with websockets.connect("ws://localhost:8000/ws/test") as ws:
+            recvv = await ws.recv()
+            print(f"{recvv}")
+    except websockets.exceptions.ConnectionClosed:
+        print("closed")
+    except Exception as e:
+        print(f"{e}")
+asyncio.run(wss())
