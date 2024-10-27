801e800e6b9f2119dccc0f70296c7a755060f16c
cyxdev <ziadatef@192.168.1.7>
2024-10-27 14:01:52 -0700

    bot at 2024-10-27 14:01:52

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ app = fastapi.FastAPI()
 
 def fetchsrc_files():
     # todo(): [fetch the file names from the src dir ]
-    print(os.listdir("src"))
-
+    # this returns a list . 
+    fileslist = os.listdir("src")
+    routes = []
+    for i in fileslist:
+        if i.endWith(".html")
+             routename = i.replace(".html", "")
+             routes.append(routename)
 
 print(fetchsrc_files())
