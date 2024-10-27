df35c56daf089adfe6186b209e73f35e1fc375aa
cyxdev <ziadatef@192.168.1.7>
2024-10-27 16:36:09 -0700

    bot at 2024-10-27 16:36:09

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ async def reciever(sessionid: str):
     functionslist = functionsFetcher()
     websocketsCreator(functionslist, sessionid)
     return {"status": "websockets created", "functions": functionslist}
-def websocketprinter(app: app):
+def websocketprinter():
     rx = [r for r in app.routes if r.endpoint.__name__.startswith("websocket_")]
     print("websocket routes:")
     for rx in r:
