b5e01fd44fa612dc22fde343969d69f749d87b13
cyxdev <ziadatef@192.168.1.7>
2024-10-27 14:02:09 -0700

    bot at 2024-10-27 14:02:09

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = fastapi.FastAPI()
 
 def fetchsrc_files():
     # todo(): [fetch the file names from the src dir ]
-    # this returns a list . 
+    # this returns a list . [os.listdir("")]
     fileslist = os.listdir("src")
     routes = []
     for i in fileslist:
